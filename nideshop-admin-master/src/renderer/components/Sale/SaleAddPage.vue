779628e4c8efdec5e8f08e932204ac64db833131
cnapollo1 <cnapollo@hotmail.com>
2018-06-13 22:53:15 +0800

    a

--- nideshop-admin-master/src/renderer/components/Sale/SaleAddPage.vue
+++ nideshop-admin-master/src/renderer/components/Sale/SaleAddPage.vue
@@ -15,11 +15,11 @@
                 <el-form ref="infoForm" :rules="infoRules" :model="infoForm" label-width="120px">
                     <el-form-item label="上级销售商" prop="parent_id">
                         <el-select v-model="infoForm.parent_id" placeholder="请选择上级分类">
-                            <el-option v-for="item in parentCategory" :key="item.id" :label="item.name" :value="item.parent_id"></el-option>
+                            <el-option v-for="item in parentSale" :key="item.id" :label="item.name" :value="item.id"></el-option>
                         </el-select>
                     </el-form-item>
-                    <el-form-item label="手机号" prop="id">
-                        <el-input v-model="infoForm.id"></el-input>
+                    <el-form-item label="手机号" prop="mobile">
+                        <el-input v-model="infoForm.mobile"></el-input>
                     </el-form-item>
                     <el-form-item label="销售商名称" prop="name">
                         <el-input v-model="infoForm.name"></el-input>
@@ -52,6 +52,9 @@
                         <el-button type="primary" @click="onSubmitInfo">确定保存</el-button>
                         <el-button @click="goBackPage">取消</el-button>
                     </el-form-item>
+                    <el-form-item>
+                      <el-hidden v-model="infoForm.isRecordNew"></el-hidden>
+                    </el-form-item>
                 </el-form>
             </div>
         </div>
@@ -59,6 +62,7 @@
 </template>
 
 <script>
+  var UUID = require('uuid');
   import api from '@/config/api';
  // this.api = api;
   export default {
@@ -68,20 +72,23 @@
         uploaderHeader: {
           'X-Nideshop-Token': localStorage.getItem('token') || '',
         },
-        parentCategory: [
+        parentSale: [
           {
-            id: 0,
-            name: '顶级经销商'
+            // id: 0,
+            // name: '平台商城'
           }
         ],
+        //是否授权(0,未 1,已 2,已申请 3,审核中 9,不予授权
         authorizeType:[
           {id:0,
             name:'未审核'},
           {id:1,
             name:'审核通过'},
           {id:2,
-            name:'审核中'},
+            name:'申请审核'},
           {id:3,
+            name:'审核中'},
+          {id:9,
             name:'不予授权'}
         ],
         infoForm: {
@@ -92,10 +99,11 @@
           team_id: 0,
           layer: 3,
           authorize: 0,
+          isRecordNew:true,
         },
         infoRules: {
           id: [
-            { required: true, message: '请输入手机号', trigger: 'blur' },
+            {type: 'number', maxlength:11, minLength:11,message: '请输入11位数字手机号', trigger: 'blur' },
           ],
           name: [
             { required: true, message: '请输入名称', trigger: 'blur' },
@@ -114,9 +122,24 @@
         this.$router.go(-1);
       },
       onSubmitInfo() {
+        var ID = UUID.v1();
+        if(this.infoForm.parent_id==this.infoForm.id){
+                this.$message({
+                  type: 'error',
+                  message: '上级经销商不能为自己'
+                })
+          return false;
+        }
         this.$refs['infoForm'].validate((valid) => {
           if (valid) {
            // this.infoForm.wap_banner_url=this.infoForm.fileName;
+           //取得销售商等级  
+            let that =this;
+             var sales=this.parentSale.filter(function(p){
+              return p.id==that.infoForm.parent_id;
+             });
+             if(sales[0])
+              this.infoForm.layer=sales[0].layer+1;
             this.axios.post('sale/store', this.infoForm).then((response) => {
               if (response.data.errno === 0) {
                 this.$message({
@@ -130,27 +153,29 @@
                   message: '保存失败'
                 })
               }
+              console.log(response);
             })
           } else {
             return false;
           }
         });
       },
-      handleUploadImageSuccess(res, file) {
-        if (res.errno === 0) {
-          switch (res.data.name) {
-            //分类图片
-            case 'wap_banner_url':
-              // this.$set('infoForm.wap_banner_url', res.data.fileUrl);
-              this.infoForm.wap_banner_url = res.data.fileUrl;
-              this.infoForm.fileName = res.data.fileName;
-              break;
-          }
-        }
-      },
-      getTopCategory() {
-        this.axios.get('sale/topSale').then((response) => {
-          this.parentCategory = this.parentCategory.concat(response.data.data);
+      // handleUploadImageSuccess(res, file) {
+      //   if (res.errno === 0) {
+      //     switch (res.data.name) {
+      //       //分类图片
+      //       case 'wap_banner_url':
+      //         // this.$set('infoForm.wap_banner_url', res.data.fileUrl);
+      //         this.infoForm.wap_banner_url = res.data.fileUrl;
+      //         this.infoForm.fileName = res.data.fileName;
+      //         break;
+      //     }
+      //   }
+      // },
+      getParentSale() {
+        this.axios.get('sale/parentSale').then((response) => {
+          // this.parentSale = this.parentSale.concat(response.data.data);
+          this.parentSale = response.data.data;
         })
       },
       getInfo() {
@@ -168,6 +193,7 @@
           let resInfo = response.data.data;
           // resInfo.is_show = resInfo.is_show ? true : false;
           that.infoForm = resInfo;
+          that.infoForm.isRecordNew=false;
           console.log(resInfo);
           console.log(that.rootHost);
           // that.infoForm.wap_banner_url=that.rootHost +that.infoForm.wap_banner_url;
@@ -183,7 +209,7 @@
     },
     components: {},
     mounted() {
-      this.getTopCategory();
+      this.getParentSale();
       this.infoForm.id = this.$route.query.id || 0;
       this.getInfo();
     }
--- server/src/admin/controller/sale.js
+++ server/src/admin/controller/sale.js
@@ -11,30 +11,37 @@ module.exports = class extends Base {
     const name = this.get('name') || '';
 
     const model = this.model('sale');
-    const dataLayer2    =await model.where({layer:1}).select();
-    const data = await model.where({name: ['like', `%${name}%`]}).order(['id DESC']).page(page, size).countSelect();
-    const saleList=[];
-    dataLayer2.map( (item)=>{
-        var isAdd=false;
-        data.data.map((child)=>{
-            if(child.parent_id==item.id){
-                if(!isAdd){
-                    saleList.push(item);
-                    isAdd=true;
-                }
-                if(child.id!=item.id)
-                    saleList.push(child);
-            }
-        });
-});
+    const dataLayer2 = await model.where({
+      layer: 2
+    }).select();
+    // const dataLayer2    =await model.where({layer:{'>',1}}).select();
+    const data = await model.where({
+      name: ['like', `%${name}%`]
+    }).order(['id DESC']).page(page, size).countSelect();
+    // const saleList = [];
+    // dataLayer2.map((item) => {
+    //   var isAdd = false;
+    //   data.data.map((child) => {
+    //     if (child.id == item.id || child.parent_id == item.id) {
+    //       if (!isAdd) {
+    //         saleList.push(item);
+    //         isAdd = true;
+    //       }
+    //       if (child.parent_id == item.id)
+    //         saleList.push(child);
+    //     }
+    //   });
+    });
     console.log(this.ctx.origin);
-    return this.success(saleList);
+    return this.success(data);
   }
 
   async infoAction() {
     const id = this.get('id');
     const model = this.model('sale');
-    const data = await model.where({id: id}).find();
+    const data = await model.where({
+      id: id
+    }).find();
 
     return this.success(data);
   }
@@ -43,42 +50,51 @@ module.exports = class extends Base {
     if (!this.isPost) {
       return false;
     }
-
+    // console.log(this.DBERR);
     const values = this.post();
     const id = this.post('id');
+    const isRecordNew = this.post('isRecordNew');
 
     const model = this.model('sale');
-    // values.is_on_sale = values.is_on_sale ? 1 : 0;
-    // values.is_new = values.is_new ? 1 : 0;
-    // values.is_hot = values.is_hot ? 1 : 0;
-    if (id > 0) {
-      await model.where({id: id}).update(values);
-    } else {
-      //delete values.id;
-      // await model.add(values);
-      //g_mod
-     // values.id=values.goods_sn>>0;
-      await model.add(values);
-    }
-    return this.success(values);
+    var result;
+    if (isRecordNew)
+      result = await model.thenAdd(values, {
+        id: 0
+      });
+    else
+      result = await model.where({
+        id: id
+      }).update(values);
+    return this.DAReturn(result, values);
+    // return this.fail(this.DBERR.ERR_INSERT_EXIST_CODE,this.DBERR.ERR_INSERT_EXIST_MSG,result);
+    // return this.success(result);
   }
 
   async destoryAction() {
     const id = this.post('id');
-    await this.model('sale').where({id: id}).limit(1).delete();
+    await this.model('sale').where({
+      id: id
+    }).limit(1).delete();
     // TODO 删除图片
     return this.success();
   }
-  async getAllCategoryAction(){
-    const model   =this.model('sale');
-      const data=await model.select();
-
-      return this.success(data);
-  }
-
-  async topSaleAction() {
+  async getAllCategoryAction() {
     const model = this.model('sale');
-    const data = await model.where({layer: 2}).order(['id ASC']).select();
+    const data = await model.select();
+
+    return this.success(data);
+  }
+  /**
+   * 取得二级经销商
+   */
+  async parentSaleAction() {
+    const model = this.model('sale');
+    // const data = await model.where({layer: 2}).order(['id ASC']).select();
+    // const data = await model.where({layer: {1,2}}).order(['id ASC']).select();
+    const data = await model.where({
+      layer: ['<', 3]
+    }).order(['id ASC']).select();
+
     return this.success(data);
   }
 };
