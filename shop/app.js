fe4474583814d5e5c5822e435b1a926656d7351f
cnapollo1 <cnapollo@hotmail.com>
2018-06-25 13:29:25 +0800

    a

--- shop/app.js
+++ shop/app.js
@@ -3,7 +3,12 @@ var api = require('./config/api.js');
 var user = require('./services/user.js');
 
 App({
-  onLaunch: function () {
+  onLaunch: function (options) {
+
+    console.log('app.js onLaunch option->');
+    this.handOptions(options);
+    // this.globalData.scene="832bb850-2162-4e42-7060796a2fb8";
+    
     //获取用户的登录信息
     user.checkLogin().then(res => {
       console.log('app login')
@@ -14,7 +19,39 @@ App({
         console.log(e)
     });
   },
-  
+
+  onShow:function (options) {
+    console.log('app onShow');
+   this.handOptions(options);
+  },
+  onHide: function (options) {
+    console.log('app onHide');
+    console.log(options);
+  },
+  //无此事件，在页面触发
+  // onShareAppMessage: function () {
+  //   return {
+  //     title: '一车一店',
+  //     desc: '微信小程序商城',
+  //     path: '/pages/index/index'
+  //   }
+  // },
+  onError: function(msg) {
+    console.log(msg)
+  },
+  handOptions:function(options){
+    console.log(options);
+    console.log(options.scene);
+
+    var scene = decodeURIComponent(options.scene)
+    this.shareTicket='';
+    if(options && options.scene&&options.scene==1044){
+      this.shareTicket=options.shareTicket?options.shareTicket:'';
+    }
+  },
+  handSecne(scene){
+
+  },
   globalData: {
     businessInfo:{
       scene:''
@@ -22,7 +59,7 @@ App({
     userInfo: {
       nickname: 'Hi,游客',
       username: '点击去登录',
-      avatar: 'http://yanxuan.nosdn.127.net/8945ae63d940cc42406c3f67019c5cb6.png'
+      avatar: '/static/images/head.png'
     },
     token: '',
   }
---
+++ uuid.test.md
@@ -0,0 +1,8 @@
+https://www.cnblogs.com/luyang08/p/6056621.html
+
+#call int_init(50000);
+191.597s
+#call uuid20_init(50000);
+192.527s
+#call uuid_init(50000);
+190.738s
